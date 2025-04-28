eb455df6528521508c1cfb4b19f4e93640a41650
SheilaDD <sheiz.divhani@gmail.com>
2025-04-28 21:33:23 +0300

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+# while loop repeats a piece of code until the condition becomes False
+
+n = 1
+while n <= 3:
+    print(n, "\n")
+    n += 1
+
+# print 5times
+
+number = 0
+while number <= 5:
+    number = number + 1
+
+    if number == 6:
+        continue
+
+    print(number, "\n", end='')
+else:
+    print("No longer < 5")
